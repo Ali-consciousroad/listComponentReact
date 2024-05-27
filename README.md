3e11d2af2e57a731cd1b2ae82764de25758701a2
ali-consciousroad <ali_dindar@live.be>
2024-05-27 16:41:25 +0200

    Explain the code in the readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+# List component
+Loop over the desserts array, the data prop to display all desserts using an ES6 map function
+
+Logic
+A. Important Filter and Sort first for better efficiency
+B. Map the data
+  1. Filter desserts with calories < 500 
+  2. Sort them by calories in ascending order
+  3. Map over the filtered and sorted desserts to generate the JSX
--- src/DessertsList.js
+++ src/DessertsList.js
@@ -1,14 +1,5 @@
 // Child component
 function DessertsList(props) {
-  // Loop over the desserts array, the data prop to display all desserts using ES6 map function
-
-  // Logic
-  // A. Important Filter and Sort first for better efficiency
-  // B. Map the data
-  // 1. Filter desserts with calories < 500 
-  // 2. Sort them by calories in ascending order
-  // 3. Map over the filtered and sorted desserts to generate the JSX
-
 // Access the desserts by working on the data property taken from the props object
   const filteredAndSortedDesserts = props.data 
     // Filter the dessert based on the calories value
